643a06438162991cf3a013b5b825fb393d86ea9e
WaheedDurrani <waheedkhan6670@gmail.com>
2024-02-01 13:12:13 +0500

    intercommunication between microservice

--- student-service/src/main/java/com/hexagon/studentservice/controller/StudentProfileController.java
+++ student-service/src/main/java/com/hexagon/studentservice/controller/StudentProfileController.java
@@ -10,7 +10,9 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.List;
 
 @CrossOrigin("*")
@@ -21,6 +23,9 @@ public class StudentProfileController {
     @Autowired
     private StudentProfileService studentProfileService; // Assume you have a service to handle business logic
 
+    @Autowired
+    private RestTemplate restTemplate;
+
 
     // Endpoint to retrieve all profiles
     @GetMapping("/getAllStudentProfiles")
@@ -28,4 +33,16 @@ public class StudentProfileController {
         List<StudentProfie> profiles = studentProfileService.getAllProfiles();
         return new ResponseEntity<>(profiles, HttpStatus.OK);
     }
+
+//    Inter communication between services
+
+    @GetMapping("/school")
+    public List<Object> getSchool() {
+        Object[] objects = restTemplate.getForObject("http://SCHOOL-SERVICE/instituteAPI/getAllProfiles", Object[].class);
+        System.out.println(objects.toString());
+        return Arrays.asList(objects);
+    }
+
+
+
 }
